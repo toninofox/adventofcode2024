dfe6046e09a8afd16a98d48b618fb08fb33aaf3f
Antonio La Gamma <toninofox@users.noreply.github.com>
2024-12-10 18:50:59 +0000

    day4 sol

--- day4_ceresSearch/index.js
+++ day4_ceresSearch/index.js
@@ -1,98 +1,71 @@
 const inputs = require('./input.js')
 
+const parseInput = (rawInput) => rawInput.split(/\r?\n/).map((row) => row.split(""))
 
-const toMatrix = word => word.split('\n').map(lines=>lines.split(""))
- 
-// check whether given cell (row, col) is a valid
-// cell or not.
-function isvalid(mat, row, col, prevRow, prevCol)
-{
- 
-    const ROW = mat.length
-    const COL = mat[0].length - 1
-    // return true if row number and column number
-    // is in range
-    return (row >= 0) && (row < ROW) &&
-        (col >= 0) && (col < COL) &&
-        !(row == prevRow && col == prevCol);
-}
- 
-// These arrays are used to get row and column
-// numbers of 8 neighboursof a given cell
-let rowNum=[0,0,1,-1, -1,1,-1,1];
-let colNum=[1,-1,0,0, -1,1,-1,1];
-const directionsX = [
-    [0, 1],  [0, -1], [1, 0], [-1, 0], 
+
+
+const directions = [
+    [0, 1],
+    [1, 0],
+    [1, 1],
+    [1, -1],
+    [0, -1],
+    [-1, 0],
+    [-1, -1],
+    [-1, 1]
 ]
- const directionsO = [
- [-1, -1], [1, 1], [-1, 1], [1, -1]]
-// A utility function to do DFS for a 2D boolean
-// matrix. It only considers the 8 neighbours as
-// adjacent vertices
-function  DFS(mat, row, col, prevRow, prevCol, word, path, index, n, result,directions)
-{
-    // return if current character doesn't match with
-    // the next character in the word
-    if (index > n || mat[row][col] != word[index])
-        return ;
-   
-    // append current character position to path
-    
-    path += (word[index]) + "(" + (row).toString()
-            + ", " + (col).toString() + ") ";
-   
-    // current character matches with the last character
-    // in the word
-    if (index == n)
-    {   
-        result.push(path)
-        return true
-    }
-   
-    // Recur for all connected neighbours
-    for (let k = 0; k < directions.length; ++k){
-        if (isvalid(mat, row + directions[k][0], col + directions[k][1],
-                    prevRow, prevCol)) {
-   
-            DFS(mat, row + directions[k][0], col + directions[k][1],
-                row, col, word, path, index + 1, n, result,directions);
-               
+const invalidCoordinates = (nx, ny, matrix, word, rows, cols) => nx < 0 || ny < 0 || nx >= rows || ny >= cols || matrix[ny][nx] !== word[i]
+const checkWord = (x, y, dx, dy, matrix, word, rows, cols) => {
+    for (let i = 0; i < word.length; i++) {
+        const nx = x + i * dx
+        const ny = y + i * dy
+        if (invalidCoordinates(nx, ny, matrix, word, rows, cols)) {
+            return false
         }
     }
+    return true
 }
- 
-// The main function to find all occurrences of the
-// word in a matrix
-function findWords(mat)
-{
+
+const sol1 = (input) => {
+    let count = 0
     const word = "XMAS"
-    const n = word.length-1
-    const result = []
-    // traverse through the all cells of given matrix
-    for (let i = 0; i < mat.length; ++i) {
-        for (let j = 0; j < mat[0].length; ++j) {
-   
-            // occurrence of first character in matrix
-            if (mat[i][j] == word[0]) {
-   
-                // check and print if path exists
-                DFS(mat, i, j, -1, -1, word, "", 0, n,result,directionsX);
-                DFS(mat, i, j, -1, -1, word, "", 0, n,result,directionsO);
+    const data = parseInput(input)
+
+    const rows = data.length
+    const cols = data[0].length
+
+    for (let x = 0; x < rows; x++) {
+        for (let y = 0; y < cols; y++) {
+            for (const [dx, dy] of directions) {
+                if (checkWord(x, y, dx, dy, data, word, rows, cols)) {
+                    count++
+                }
             }
         }
     }
-    return result
-}
- 
-
-const sol1 = (input)=> {
-    return findWords(toMatrix(input));
+    return count
 }
 
-const sol2 = (input)=> {
-    const cleaned = input.replace(/(don't\(\).+?do\(\))/gs,"")
-    const formulas = findFormula(cleaned)
-    return formulas.reduce((acc,v)=> acc + (v.first * v.second),0)
+const sol2 = (input) => {
+    let count = 0
+    const data = parseInput(input)
+
+    const rows = data.length
+    const cols = data[0].length
+    for (let x = 1; x < rows - 1; x++) {
+        for (let y = 1; y < cols - 1; y++) {
+            if (data[x][y] === "A") {
+                const tlbr =
+                    (data[x - 1][y - 1] === "M" && data[x + 1][y + 1] === "S") ||
+                    (data[x - 1][y - 1] === "S" && data[x + 1][y + 1] === "M")
+                const trbl =
+                    (data[x - 1][y + 1] === "M" && data[x + 1][y - 1] === "S") ||
+                    (data[x - 1][y + 1] === "S" && data[x + 1][y - 1] === "M")
+                if (tlbr && trbl) count++
+            }
+        }
+    }
+    return count
 }
 
-console.log(sol1(inputs.i1))
+console.log(sol2(inputs.i2))
